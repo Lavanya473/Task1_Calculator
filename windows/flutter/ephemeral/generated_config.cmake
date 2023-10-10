b7cd1108186e136a538dc2ba161228ecc187a74e
Lavanya473 <108178398+Lavanya473@users.noreply.github.com>
2023-10-10 20:47:23 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,25 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\LAVANYA\\Downloads\\flutter_windows_3.0.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\LAVANYA\\Desktop\\Calculator\\flutter_application_calculator" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\LAVANYA\\Downloads\\flutter_windows_3.0.2-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\LAVANYA\\Desktop\\Calculator\\flutter_application_calculator"
+  "FLUTTER_ROOT=C:\\Users\\LAVANYA\\Downloads\\flutter_windows_3.0.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\LAVANYA\\Desktop\\Calculator\\flutter_application_calculator\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\LAVANYA\\Desktop\\Calculator\\flutter_application_calculator"
+  "FLUTTER_TARGET=C:\\Users\\LAVANYA\\Desktop\\Calculator\\flutter_application_calculator\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\LAVANYA\\Desktop\\Calculator\\flutter_application_calculator\\.dart_tool\\package_config.json"
+)
